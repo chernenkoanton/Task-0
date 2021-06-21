72821bbff325df1a474787ebfa45a317cdccba96
chernenkoanton <86268085+chernenkoanton@users.noreply.github.com>
2021-06-22 02:10:44 +0300

    Update Task 0.py

--- Task0/Task 0.py
+++ Task0/Task 0.py
@@ -1,10 +1,10 @@
 import random
-a = [random.randint(-100, 100) for x in range(30)]
-biggest = max(a)
+p = [random.randint(-100, 100) for x in range(30)]
+biggest = max(p)
 r.index(biggest_number)
-print(a)
+print(p)
 print(biggest)
-print(a.index(biggest_number)+1)
+print(p.index(biggest_number)+1)
 for i in range(29):
-  if a[i]<0 and a[i+1]<0:
-    print(a[i],a[i+1])
+  if p[i]<0 and p[i+1]<0:
+    print(p[i],p[i+1])
